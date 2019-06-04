d3f74a3827abbc727cfea6dcbdd72494591ce152
juwanpetty <jchpetty@gmail.com>
2019-06-04 08:24:22 -0400

    Remove cursor and paper.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,10 @@ import React from "react"
 import { Header, Footer } from "../../components"
 
 import styles from "./Layout.module.scss"
-import "../Cursor/cursor.scss"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <canvas class="canvas" data-paper-resize />
       <div className={styles.Container}>
         <Header />
         {children}
