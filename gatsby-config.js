b5977afcf3a6e07f30ee2ee6604314e27f03a0a8
juwanpetty <jchpetty@gmail.com>
2019-06-05 21:49:12 -0400

    Update project layout style

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/, // See below to configure properly
-        },
-      },
-    },
     // {
     //   resolve: "gatsby-source-contentful",
     //   options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { ArrowRight } from "react-feather"
 import { Layout } from "../components"
 
 import styles from "./index.module.scss"
-import SelectedProjects from "../assets/selected-projects.svg"
 
 const Index = () => {
   // const data = useStaticQuery(graphql`
@@ -27,35 +27,33 @@ const Index = () => {
           <h1 className={styles.SectionHeader}>Selected Projects</h1>
           <ul className={styles.Projects}>
             <li className={styles.Project}>
-              <a href="/">
+              <div className={styles.Image} />
+              <div className={styles.Meta}>
+                <h1 className={styles.Title}>Headlines</h1>
                 <p className={styles.Description}>
-                  Personal Project — Chrome Extension
+                  Love Instax Mini photos? We like them too. That's what
+                  inspired us to create Tookapic Mini Prints! Cute little cards
+                  with your photos and a short note.
                 </p>
-                <p className={styles.Title}>Headlines</p>
-              </a>
-            </li>
-            <li className={styles.Project}>
-              <a href="/">
-                <p className={styles.Description}>
-                  Personal Project — Chrome Extension
+                <p className={styles.Detail}>
+                  March 2018
+                  <br />
+                  Chrome Extension
                 </p>
-                <p className={styles.Title}>Picture in Picture</p>
-              </a>
+                <a className={styles.Link} href="">
+                  Link to project
+                  <ArrowRight className={styles.LinkIcon} size={20} />
+                </a>
+              </div>
+              <div className={styles.Image} />
+              <div className={styles.Image} />
             </li>
           </ul>
         </section>
-        {/* <SelectedProjects className={styles.SelectedProjects} />
-      <ul className={styles.Projects}>
-        {data.allContentfulProject.edges.map(edge => (
-          <li data-cursor key={edge.node.title} className={styles.Project}>
-            <a href={edge.node.url}>{edge.node.title}</a>
-          </li>
-        ))}
-      </ul> */}
         <section className={styles.Section}>
           <h1 className={styles.SectionHeader}>Work</h1>
-          <ul className={styles.Projects}>
-            <li className={styles.Project}>
+          <ul className={styles.Jobs}>
+            <li className={styles.Job}>
               <a href="/">
                 <p className={styles.Description}>September 2018 — May 2019</p>
                 <p className={styles.Title}>Shopify</p>
