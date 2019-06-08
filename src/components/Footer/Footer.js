450bab0aa92bc27b73498c0e17c8dbc9cd2b3ae5
juwanpetty <jchpetty@gmail.com>
2019-06-07 20:47:26 -0400

    Remove about page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,6 @@ import styles from "./Footer.module.scss"
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <Link to="/about" data-cursor className={styles.FooterLink}>
-        About
-      </Link>
       <Link to="/" data-cursor className={styles.FooterLink}>
         CV
       </Link>
