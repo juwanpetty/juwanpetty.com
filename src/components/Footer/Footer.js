d051f8067ccf1e823fae742a09ff7c0290fbdeb8
juwanpetty <jchpetty@gmail.com>
2019-06-08 10:05:19 -0400

    Add resume to site

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import Resume from "../../assets/juwan-petty-resume.pdf"
 import styles from "./Footer.module.scss"
 
 const Footer = () => {
+  console.log(Resume)
+
   return (
     <footer className={styles.Footer}>
-      <Link to="/" data-cursor className={styles.FooterLink}>
+      <a href={Resume} data-cursor className={styles.FooterLink} download>
         CV
-      </Link>
+      </a>
     </footer>
   )
 }
