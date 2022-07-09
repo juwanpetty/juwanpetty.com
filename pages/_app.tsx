3e1b52eb1204e129b4efec0d7ec0481055ed5daf
“juwanpetty” <jchpetty@gmail.com>
2022-07-09 10:27:36 -0400

    Add axe-core to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-import "../styles/styles.scss";
+import React from "react";
 import type { AppProps } from "next/app";
+import "../styles/styles.scss";
+
+if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
+  const ReactDOM = require("react-dom");
+  const axe = require("@axe-core/react");
+  axe(React, ReactDOM, 1000);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
