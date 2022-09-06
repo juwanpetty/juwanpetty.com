2721c841c35df1d58d9e195d0f62d2367a77b0ed
“juwanpetty” <jchpetty@gmail.com>
2022-09-06 17:13:35 -0400

    put html elements into styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Head from "next/head";
 
 import { Subscribe } from "components";
+import styled from "styled-components";
 
 const Index: NextPage = () => {
   return (
@@ -15,49 +16,47 @@ const Index: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div
-        style={{
-          display: "flex",
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "96px",
-            fontWeight: 700,
-          }}
-        >
-          Coming Soon
-        </h1>
-        <p
-          style={{
-            fontSize: "18px",
-            textAlign: "center",
-            marginBottom: "6rem",
-          }}
-        >
-          From automation of people processes to creating <br /> an engaged and
-          driven culture.
-        </p>
+      <Container>
+        <Title>Coming Soon</Title>
+        <Subtitle>
+          Something’s stirring in this corner of the internet. <br />
+          Add your name to the email list to be notified when the site goes
+          live.
+        </Subtitle>
+
         <Subscribe />
 
-        <p
-          style={{
-            fontSize: "0.875rem",
-            lineHeight: "1.25rem",
-            color: "rgb(107 114 128)",
-            textAlign: "center",
-            marginTop: "0.75rem",
-          }}
-        >
-          I&apos;ll only use this for spam. Kidding.
-        </p>
-      </div>
+        <Caption>I&apos;ll only use this for spam. Kidding.</Caption>
+      </Container>
     </>
   );
 };
 
 export default Index;
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 96px;
+  font-weight: 700;
+`;
+
+const Subtitle = styled.p`
+  font-size: 18px;
+  text-align: center;
+  margin-bottom: 6rem;
+`;
+
+const Caption = styled.p`
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  color: rgb(107 114 128);
+  text-align: center;
+  margin-top: 0.75rem;
+`;
