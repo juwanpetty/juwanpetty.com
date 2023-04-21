d87c9f648a2c6918d90036397600bf604a7efe23
Juwan Petty <jchpetty@gmail.com>
2023-04-20 21:14:12 -0400

    fresh start

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import localFont from "@next/font/local";
 import type { AppProps } from "next/app";
-
-import Layout from "@/components/Layout";
-import classNames from "classnames";
 
 import "../styles/globals.scss";
 
@@ -14,60 +10,8 @@ if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
   axe(React, ReactDOM, 1000);
 }
 
-const grotesk = localFont({
-  src: [
-    {
-      path: "../public/fonts/neufile-grotesk-regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-regular-italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-medium-italic.ttf",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-semibold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-semibold-italic.ttf",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/neufile-grotesk-bold-italic.ttf",
-      weight: "700",
-      style: "italic",
-    },
-  ],
-  variable: "--font-grotesk",
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className={classNames("font-sans", grotesk.variable)}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
