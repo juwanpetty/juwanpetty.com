a6e456b7fa3ed6cc2a1151c718088f7817bb86af
Juwan Petty <jchpetty@gmail.com>
2024-06-05 17:54:33 -0400

    Update sidebar and header heights to use CSS variables

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export function Aside({ children }: Props) {
   return (
-    <aside className="sticky top-10 hidden h-[calc(100vh-3.5rem)] w-56 shrink-0 flex-col gap-10 overflow-y-auto p-6 pb-16 pl-4 lg:flex">
+    <aside className="sticky top-10 hidden h-[calc(100vh-var(--header-height))] w-56 shrink-0 flex-col gap-10 overflow-y-auto p-6 pb-16 pl-4 lg:flex">
       {children}
       <ScrollToTop />
     </aside>
