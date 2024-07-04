c14c8d415c0416c40716b834e33ad7cc89c3b54a
Juwan Petty <jchpetty@gmail.com>
2024-07-03 21:11:09 -0400

    refactor: Update IconButton component to use utility function for merging class names

--- shared/components/IconButton.tsx
+++ shared/components/IconButton.tsx
@@ -1,5 +1,6 @@
 import { ButtonHTMLAttributes, createElement } from "react";
 import { Icons } from "@/shared/Icons";
+import { cn } from "@/utilities/mergeClassNames";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
@@ -9,8 +10,15 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function IconButton({ label, icon, className, ...props }: Props) {
   return (
-    <button type="button" className={className} {...props}>
-      {createElement(Icons[icon])}
+    <button
+      type="button"
+      className={cn(
+        "inner-shadow flex size-8 items-center justify-center rounded-lg border border-solid border-stone-200 drop-shadow-sm",
+        className
+      )}
+      {...props}
+    >
+      {createElement(Icons[icon], { className: "size-4" })}
       <span className="sr-only">{label}</span>
     </button>
   );
