5dd2ec39e057c15d2f58ad812523e2cd0893491c
Juwan Petty <jchpetty@gmail.com>
2025-01-30 14:21:13 -0500

    Add type annotations for anchor props in PostMDXComponents

--- src/features/post/post-mdx-components.tsx
+++ src/features/post/post-mdx-components.tsx
@@ -1,5 +1,7 @@
+import { AnchorHTMLAttributes } from "react";
+
 export const PostMDXComponents = {
-  a: (props) => (
+  a: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
     <a
       {...props}
       className="font-normal underline decoration-neutral-200 underline-offset-3 transition-colors duration-200 hover:decoration-neutral-600"
