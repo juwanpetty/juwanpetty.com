af6d74832d7b1a22914277c9f50a05ee31612688
Juwan Petty <jchpetty@gmail.com>
2025-10-16 13:03:54 -0400

    Project file cleanup (#124)
    * Remove unused font files
    
    * Remove unused MDX utility functions in favor of content collections
    
    * Remove unused next-mdx-remote dependency and related configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,13 @@
 import nextMDX from "@next/mdx";
-import rehypePrettyCode from "rehype-pretty-code";
 import { withContentCollections } from "@content-collections/next";
-
-/** @type {import('rehype-pretty-code').Options} */
-const options = {
-  theme: {
-    dark: "github-dark",
-    light: "github-light",
-  },
-  keepBackground: false,
-};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["next-mdx-remote"],
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 const withMDX = nextMDX({
   extension: /\.(md|mdx)$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
 });
 
 export default withContentCollections(withMDX(nextConfig));
