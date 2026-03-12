8b42d0eba6d42132481ae56ac1d6279a9daa18a7
Juwan Petty <jchpetty@gmail.com>
2026-03-12 16:26:05 -0400

    chore: update dependencies and add MDX support
    - Updated @content-collections/core to version 0.14.2 and added @content-collections/mdx as a dependency.
    - Removed unnecessary ESLint disable comment in button component.
    - Added ESLint configuration file for custom rules and ignores.
    - Created mdx-components file to define custom MDX components for rendering.
    - Implemented dynamic blog article detail page with MDX content rendering.
    - Added static blog page with example content.
    - Created new article on microinteractions with user-centric design principles.

---
+++ mdx-components.tsx
@@ -0,0 +1,60 @@
+import { cn } from "@/lib/utils";
+
+export const mdxComponents = {
+  h1: ({ className, ...props }: React.ComponentProps<"h1">) => (
+    <h1
+      className={cn(
+        "font-heading mt-2 scroll-m-28 text-3xl font-bold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: React.ComponentProps<"h2">) => {
+    return (
+      <h2
+        id={props.children
+          ?.toString()
+          .replace(/ /g, "-")
+          .replace(/'/g, "")
+          .replace(/\?/g, "")
+          .toLowerCase()}
+        className={cn(
+          "font-heading [&+]*:[code]:text-xl mt-10 scroll-m-28 text-xl font-medium tracking-tight first:mt-0 lg:mt-12 [&+.steps]:mt-0! [&+.steps>h3]:mt-4! [&+h3]:mt-6! [&+p]:mt-4!",
+          className
+        )}
+        {...props}
+      />
+    );
+  },
+  a: ({ className, ...props }: React.ComponentProps<"a">) => (
+    <a
+      className={cn("font-medium underline underline-offset-4", className)}
+      {...props}
+    />
+  ),
+  p: ({ className, ...props }: React.ComponentProps<"p">) => (
+    <p
+      className={cn("text-base/7 [&:not(:first-child)]:mt-6", className)}
+      {...props}
+    />
+  ),
+  strong: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <strong className={cn("font-medium", className)} {...props} />
+  ),
+  ul: ({ className, ...props }: React.ComponentProps<"ul">) => (
+    <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+  ),
+  ol: ({ className, ...props }: React.ComponentProps<"ol">) => (
+    <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+  ),
+  li: ({ className, ...props }: React.ComponentProps<"li">) => (
+    <li className={cn("mt-2", className)} {...props} />
+  ),
+  blockquote: ({ className, ...props }: React.ComponentProps<"blockquote">) => (
+    <blockquote
+      className={cn("mt-6 border-l-2 pl-6 italic", className)}
+      {...props}
+    />
+  ),
+};
