a6b8216ac8aeb6a582c6fd689e213377d14c34c5
Juwan Petty <jchpetty@gmail.com>
2026-03-21 01:04:59 -0400

    Refactor crafts to patterns, update components and remove unused files (#164)

--- src/components/pattern-item.tsx
+++ src/components/pattern-item.tsx
@@ -1,15 +1,15 @@
-import type { Craft } from "content-collections";
+import type { Pattern } from "content-collections";
 import Image from "next/image";
 import Link from "next/link";
 
-type CraftItemProps = {
-  craft: Craft;
+type PatternItemProps = {
+  pattern: Pattern;
 };
 
-export function CraftItem({ craft }: CraftItemProps) {
-  const { title } = craft;
-  const path = craft._meta.path;
-  const fileSrc = `/crafts/${path}.png`;
+export function PatternItem({ pattern }: PatternItemProps) {
+  const { title } = pattern;
+  const path = pattern._meta.path;
+  const fileSrc = `/patterns/${path}.png`;
 
   return (
     <div className="border-border bg-muted relative aspect-[4/3] rounded-2xl border p-0.5 pb-0">
@@ -25,7 +25,7 @@ export function CraftItem({ craft }: CraftItemProps) {
 
       <div className="flex items-center justify-between px-3 py-2.5">
         <h3 className="text-foreground text-base font-medium tracking-tight">
-          <Link href={`/crafts/${path}`}>
+          <Link href={`/patterns/${path}`}>
             {title}
             <span className="absolute inset-0" />
           </Link>
