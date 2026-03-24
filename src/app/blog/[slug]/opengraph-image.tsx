0d9f4f8585cb11ace71b297716ec88aa0e8bc048
Juwan Petty <jchpetty@gmail.com>
2026-03-23 22:32:37 -0400

    Add Open Graph image component for blog posts

---
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,46 @@
+import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { allPosts } from "content-collections";
+
+export const alt = "Juwan Petty";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const interBold = await readFile(
+    join(process.cwd(), "public/assets/fonts/Inter-Bold.ttf")
+  );
+
+  const { slug } = await params;
+  const post = allPosts.find((post) => post._meta.path === slug);
+
+  return new ImageResponse(
+    (
+      <div tw="flex w-full h-full p-20 py-22.5 flex-col bg-neutral-50 justify-end">
+        <h1 tw="font-sans text-neutral-900 text-7xl font-bold leading-[1.2] text-balance">
+          {post?.title || "Juwan Petty"}
+        </h1>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: interBold,
+          style: "normal",
+          weight: 700,
+        },
+      ],
+    }
+  );
+}
