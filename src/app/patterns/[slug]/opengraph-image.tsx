3f404724effb43e0e3f602552e5e27bc6c0d7a7d
Juwan Petty <jchpetty@gmail.com>
2026-03-24 10:19:57 -0400

    Add Open Graph image and alt text; remove image generation component

--- src/app/patterns/[slug]/opengraph-image.tsx
+++ src/app/patterns/[slug]/opengraph-image.tsx
@@ -16,19 +16,30 @@ export default async function Image({
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const interBold = await readFile(
-    join(process.cwd(), "public/assets/fonts/Inter-Bold.ttf")
-  );
+  const [interBold, bgImage] = await Promise.all([
+    readFile(join(process.cwd(), "public/assets/fonts/Inter-Bold.ttf")),
+    readFile(join(process.cwd(), "public/assets/images/og-image.png")),
+  ]);
+
+  const bgImageSrc = `data:image/png;base64,${bgImage.toString("base64")}`;
 
   const { slug } = await params;
   const pattern = allPatterns.find((pattern) => pattern._meta.path === slug);
 
   return new ImageResponse(
     (
-      <div tw="flex w-full h-full p-20 py-22.5 flex-col bg-neutral-50 justify-end">
-        <h1 tw="font-sans text-neutral-900 text-7xl font-bold leading-[1.2] text-balance">
-          {pattern?.title || "Juwan Petty"}
-        </h1>
+      <div tw="flex w-full h-full relative">
+        <img
+          src={bgImageSrc}
+          tw="absolute inset-0 w-full h-full object-cover"
+          alt={pattern?.title || "Juwan Petty"}
+        />
+
+        <div tw="flex w-full h-full p-17.5 pb-22.5 flex-col justify-end relative">
+          <h1 tw="font-sans text-white text-7xl font-bold leading-[1.2] text-balance">
+            {pattern?.title || "Juwan Petty"}
+          </h1>
+        </div>
       </div>
     ),
     {
